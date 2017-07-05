8a92c1880a528d8d12fcbce13cd8a632eae1ca9e
n3m3sis42 <n3m3sis42@gmail.com>
2017-07-05 19:07:41 -0400

    done

--- spec/models/student_spec.rb
+++ spec/models/student_spec.rb
@@ -1,3 +1,14 @@
+# == Schema Information
+#
+# Table name: students
+#
+#  id         :integer          not null, primary key
+#  first_name :string           not null
+#  last_name  :string           not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 require 'rails_helper'
 
 describe Student do
